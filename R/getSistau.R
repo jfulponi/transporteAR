4620d55154afe594a96ccd9b0c490808302d8d2d
juanifulponi <61839284+juanifulponi@users.noreply.github.com>
2020-06-06 15:39:46 -0300

    Update getSistau.R

--- R/getSistau.R
+++ R/getSistau.R
@@ -1,18 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
 #' @title getSistau
 #' @description Get SISTAU data by period.
 #' @param annual `TRUE` or `FALSE`. If `TRUE`, returns annual data. If `FALSE` returns monthly data., Default: `TRUE`.
